15efa579e71d3d976aef040b6d8538ca8ea3225e
Arun <arun07aj@gmail.com>
2024-02-06 23:11:00 +0530

    initial springboot commit

---
+++ src/test/java/com/hsl/tokenmanagementservice/TokenManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsl.tokenmanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
