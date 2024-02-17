510cd605826d8f0a48e1516205d7a2c4580d9fdf
Arun <arun07aj@gmail.com>
2024-02-17 19:55:39 +0530

    added logger util cls for slf4j app logging

---
+++ src/main/java/com/hsl/tokenmanagementservice/util/LoggerUtil.java
@@ -0,0 +1,9 @@
+package com.hsl.tokenmanagementservice.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+public class LoggerUtil {
+    public static Logger getLogger(Class<?> cls) {
+        return LoggerFactory.getLogger(cls);
+    }
+}
